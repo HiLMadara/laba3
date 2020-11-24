592c8653ae364e1879f208d6601ff34f1360c447
HiLMadara <71180913+HiLMadara@users.noreply.github.com>
2020-11-24 14:49:39 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,8 @@
 int proverka(char* a) {
 	int rf = 0;
 	for (int i = 0; a[i] != 0; i++) {
-		if (a[i] >= '0' && a[i] <= '9' || a[i] == '-') continue;
-		else rf = 1;
+		if (a[i] < '0' || a[i] > '9' || a[i] == '-') rf = 1;
+		else continue;
 	}
 	return rf;
 }
@@ -26,13 +26,20 @@ int main() {
 	char str1[1000] = "",str2[1000] = "" ;
 	printf("Vvedite razmer massiva ");
 		fgets(str1, 1000, stdin);
-		proverka(str1);
+		if (proverka(str1) == 1)
+		{
+			printf("ERROR!Insert correct value:\n");
+		}
 		int i;
 		int x = myAtoi(str1);
 		for (i = 0; x - 1; i++) {
 			int a[]={0};
 			fgets(str2, 1000, stdin);
-			proverka(str2);
+			if (proverka(str2) == 1)
+			{
+				printf("ERROR!Insert correct value:\n");
+				break;
+			}
 			a[i] = myAtoi(str2);
 			printf("%d\n", a[i]);
 		}
